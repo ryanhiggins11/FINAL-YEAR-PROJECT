d6d6c09286942b8c5b9abfcf7a8ae4239210c7a3
Daniel Gallagher <46001378+DanielGallagher6499@users.noreply.github.com>
2020-12-10 18:24:46 +0000

    New logo added , Drawer view added and functionality added.

--- FYP/app/src/main/java/com/gmit/biztech/MainActivity.kt
+++ FYP/app/src/main/java/com/gmit/biztech/MainActivity.kt
@@ -2,13 +2,43 @@ package com.gmit.biztech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Welcome",Toast.LENGTH_SHORT).show()
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.employeeItem -> Toast.makeText(applicationContext,
+                "Employee Selected", Toast.LENGTH_SHORT).show()
+
+                R.id.managerItem -> Toast.makeText(applicationContext,
+                    "Manager Selected", Toast.LENGTH_SHORT).show()
+
+                R.id.settingsItem -> Toast.makeText(applicationContext,
+                    "Settings Selected", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
